fb94a831165ca332dcc41f540efe196d3c3e9f84
kolosovpetro <kolosovp94@gmail.com>
2025-05-26 12:14:12 +0200

    changelog | refactors

--- main.tf
+++ main.tf
@@ -86,19 +86,23 @@ resource "azurerm_private_dns_zone" "acr_dns" {
   resource_group_name = azurerm_resource_group.public.name
 }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "acr_dns_link" {
-  name                  = "acr-dns-link-${var.prefix}"
-  resource_group_name   = azurerm_resource_group.public.name
-  private_dns_zone_name = azurerm_private_dns_zone.acr_dns.name
-  virtual_network_id    = azurerm_virtual_network.public.id
-}
-
 resource "azurerm_private_dns_a_record" "acr_dns_record" {
   name                = azurerm_container_registry.acr.name
   zone_name           = azurerm_private_dns_zone.acr_dns.name
   resource_group_name = azurerm_resource_group.public.name
   ttl                 = 300
   records             = [azurerm_private_endpoint.acr_pe.private_service_connection[0].private_ip_address]
+}
+
+#################################################################################################################
+# PRIVATE DNS ZONE VNET LINK
+#################################################################################################################
+
+resource "azurerm_private_dns_zone_virtual_network_link" "acr_dns_link" {
+  name                  = "acr-dns-link-${var.prefix}"
+  resource_group_name   = azurerm_resource_group.public.name
+  private_dns_zone_name = azurerm_private_dns_zone.acr_dns.name
+  virtual_network_id    = azurerm_virtual_network.public.id
 }
 
 #################################################################################################################
